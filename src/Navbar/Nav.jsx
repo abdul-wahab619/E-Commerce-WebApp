d473d800edae4202a6d8d3bc9468a9fa1ea433a8
abdul-wahab619 <awminhas619@gmail.com>
2024-04-05 17:15:27 +0500

    authentication in auth0

--- src/Navbar/Nav.jsx
+++ src/Navbar/Nav.jsx
@@ -1,35 +1,68 @@
+import { useState, useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import { FiHeart } from "react-icons/fi";
 import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./Nav.css";
 import DarkMode from "../components/DarkMode/DarkMode";
-
 const Nav = ({ handleInputChange, query }) => {
+  const { user, isAuthenticated, isLoading, loginWithRedirect, logout } =
+    useAuth0();
+
+  if (isLoading) {
+    return <div>Loading....</div>;
+  }
+
   return (
-    <nav>
-      <div className="nav-container">
-        <input
-          className="search-input"
-          type="text"
-          onChange={handleInputChange}
-          value={query}
-          placeholder="Enter your search shoes."
-        />
-      </div>
-      <div className="profile-container">
-        <a href="#">
-          <FiHeart className="nav-icons" />
-        </a>
-        <a href="">
-          <AiOutlineShoppingCart className="nav-icons" />
-        </a>
-        <a href="">
-          <AiOutlineUserAdd className="nav-icons" />
-        </a>
-        <a href="">
-          <DarkMode className="nav-icons" />
-        </a>
-      </div>
-    </nav>
+    <>
+      <ToastContainer />
+      <nav>
+        <div className="nav-container">
+          {isAuthenticated && <h5>Hello {user.name}</h5>}
+          <input
+            className="search-input"
+            type="text"
+            onChange={handleInputChange}
+            value={query}
+            placeholder="Enter your search shoes."
+          />
+        </div>
+        <div className="profile-container">
+          <a href="#">
+            <FiHeart className="nav-icons" />
+          </a>
+          <a href="">
+            <AiOutlineShoppingCart className="nav-icons" />
+          </a>
+          <a href="">
+            <AiOutlineUserAdd className="nav-icons" />
+          </a>
+          {isAuthenticated ? (
+            <button
+              onClick={(e) => {
+                toast.success("Successfully logged out");
+                logout();
+              }}
+            >
+              Logout
+            </button>
+          ) : (
+            <button
+              onClick={(e) => {
+                toast.success("Successfully logged in");
+                loginWithRedirect();
+              }}
+            >
+              Login
+            </button>
+          )}
+          <a href="">
+            <DarkMode className="nav-icons" />
+          </a>
+        </div>
+      </nav>
+    </>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain="abdulwahabauth0.us.auth0.com"
+      clientId="iLUw71hw8XMxtUH98UgQeXhGVPfFDcns"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
